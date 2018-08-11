ba52784e7b3bc6ab44fe6fb31043797c25268b23
digoal <digoal@126.com>
2018-08-11 17:30:10 +0800

    improve

--- generate_readme.sh
+++ generate_readme.sh
@@ -9,26 +9,24 @@
 # 迁移从163 blog 63页 <数据挖掘学习站点收集>开始算新文章迁移到本级目录,之前的算老文章迁移到old_blogs_from_163
 
 > ./README.md
-echo "<a rel=\"nofollow\" href=\"http://info.flagcounter.com/h9V1\"  ><img src=\"http://s03.flagcounter.com/count/h9V1/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_12/viewers_0/labels_0/pageviews_0/flags_0/\"  alt=\"Flag Counter\"  border=\"0\"  ></a>  " > ./README.md
-echo "  " >> ./README.md
-echo "### PostgreSQL Greenplum 培训视频分享  " >> ./README.md
+echo "### PostgreSQL, Greenplum 培训视频下载  " >> ./README.md
 echo "  "  >> ./README.md
-echo "http://pan.baidu.com/s/1pKVCgHX  ,  如果连接失效请通知我, 谢谢  " >> ./README.md
+echo "http://pan.baidu.com/s/1pKVCgHX  ,  链接失效请通知我, 谢谢  " >> ./README.md
 echo "  "  >> ./README.md
 echo "![pic](./pic/pg_china_digoal_video.jpg)  " >> ./README.md
 echo "  "  >> ./README.md
-echo "个人水平有限, 如有错误, 万望指正, 非常感谢.  "  >> ./README.md
+echo "[《阿里云 PostgreSQL 产品生态；案例、开发实践、管理实践、学习资料、学习视频 - 珍藏级》](201801/20180121_01.md) " >> ./README.md 
 echo "  "  >> ./README.md
-echo "欢迎转载(注明出处), 如有问题, 可以发issue讨论, 或微信联系我  " >> ./README.md
-echo "  "  >> ./README.md
-echo "### 二维码  " >> ./README.md
+echo "### 社区、个人微信二维码  " >> ./README.md
 echo "![pic](./pic/pg_weixin.jpg)  " >> ./README.md
 echo "  "  >> ./README.md
-echo "### digoal,德哥的PostgreSQL私房菜  " >> ./README.md
-echo "#### 老文章 : [进入](old_blogs_from_163/README.md) " >> ./README.md
-echo "### 已归类文档如下(归档还在紧锣密鼓的进行中,客官请稍等)  " >> ./README.md
+echo "如有错误, 万望指正, 非常感谢.  "  >> ./README.md
+echo "  "  >> ./README.md
+echo "欢迎转载(注明出处), 如有问题, 请发issue讨论或微信与我联系, 定抽空尽快回复  " >> ./README.md
+echo "  "  >> ./README.md
+echo "### 已归类文档如下(归档进行中... ...)  " >> ./README.md
 sed 's/](/](class\//g' class/README.md >> ./README.md
-echo "### 未归类文档如下  " >> ./README.md
+echo "### 所有文档如下  " >> ./README.md
 
 for dir in `ls -lr|awk '{print $9}'|grep -E '^[0-9]{6}'` 
 do
@@ -53,6 +51,12 @@ do
   done
   cd ..
 done
+
+echo "### digoal,德哥的PostgreSQL私房菜  " >> ./README.md
+echo "#### 老文章 : [进入](old_blogs_from_163/README.md) " >> ./README.md
+
+echo "<a rel=\"nofollow\" href=\"http://info.flagcounter.com/h9V1\"  ><img src=\"http://s03.flagcounter.com/count/h9V1/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_12/viewers_0/labels_0/pageviews_0/flags_0/\"  alt=\"Flag Counter\"  border=\"0\"  ></a>  " > ./README.md
+echo "  " >> ./README.md
 
 cd old_blogs_from_163
 . ./generate_readme.sh

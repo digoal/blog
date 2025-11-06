1dc5e013805990bdbf82fa17b6faddca4c59069d
digoal <digoal@5c1bf480a210.com>
2025-11-06 20:36:13 +0800

    new doc

--- generate_readme.sh
+++ generate_readme.sh
@@ -44,7 +44,7 @@ echo "22、[《2025-LangChain 源码入门学习21篇 汇总》](202510/20251021
 echo "23、[《2025-DuckDB 源码入门学习27篇 汇总》](202510/20251024_17.md)     "  >> ./README.md    
 echo "24、[《2025-Milvus 源码入门学习31篇 汇总》](202510/20251029_08.md)     "  >> ./README.md    
 echo "25、[《2025-VectorChord 源码入门学习39篇 汇总》](202511/20251103_17.md)     "  >> ./README.md    
-echo "26、[《2025-pgvector 源码入门学习26篇 汇总》](202511/20251105_17.md)     "  >> ./README.md    
+echo "26、[《2025-pgvector 源码入门学习27篇 汇总》](202511/20251105_17.md)     "  >> ./README.md    
 echo "  "  >> ./README.md
 echo "### 二、学习资料  " >> ./README.md
 echo "  "  >> ./README.md
